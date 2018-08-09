967da9d824b90ff0c1fa6a20f2bb65e50cd2965a
xibeifeng <huang2009303513@gmail.com>
2018-08-09 23:45:53 +0800

    自动装配

---
+++ SpringBeans的自动装配和基于注释的配置/src/byType/zidongzhuangpei/SpellChecker.java
@@ -0,0 +1,12 @@
+package byType.zidongzhuangpei;
+
+public class SpellChecker {
+    public SpellChecker(){
+        System.out.println("SpellChecker初始化");
+    }
+
+    public void checkSpelling(){
+        System.out.println("正在核实语音");
+    }
+
+}
---
+++ SpringBeans的自动装配和基于注释的配置/src/byType/zidongzhuangpei/TextEditor.java
@@ -0,0 +1,28 @@
+package byType.zidongzhuangpei;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+    private String name;
+
+
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    public void setSpellChecker(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void spellCheck(){
+
+        spellChecker.checkSpelling();
+    }
+}
