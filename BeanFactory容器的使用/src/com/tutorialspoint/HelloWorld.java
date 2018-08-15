71d83e2b18a03442ce38d730cef5eba28a2884ec
xibeifeng <huang2009303513@gmail.com>
2018-08-15 14:03:26 +0800

    InstantiationAwareBeanPostProcessor协议

--- BeanFactory容器的使用/src/com/tutorialspoint/HelloWorld.java
+++ BeanFactory容器的使用/src/com/tutorialspoint/HelloWorld.java
@@ -1,6 +1,11 @@
 package com.tutorialspoint;
 
-public class HelloWorld {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+
+public class HelloWorld implements InstantiationAwareBeanPostProcessor,BeanPostProcessor {
 
     private String message;
 
@@ -10,5 +15,42 @@ public class HelloWorld {
 
     public void getMessage(){
         System.out.println("Your Message :" + message);
+    }
+
+
+
+
+
+    //==================InstantiationAwareBeanPostProcessor协议方法==================
+    @java.lang.Override
+    public java.lang.Object postProcessBeforeInstantiation(java.lang.Class<?> aClass, java.lang.String s) throws BeansException {
+
+        System.out.println("postProcessBeforeInstantiation 方法调用");
+
+        return null;
+    }
+
+    @java.lang.Override
+    public boolean postProcessAfterInstantiation(java.lang.Object o, java.lang.String s) throws BeansException {
+        System.out.println("postProcessAfterInstantiation 方法调用");
+        return false;
+    }
+
+    @java.lang.Override
+    public PropertyValues postProcessPropertyValues(PropertyValues propertyValues, java.beans.PropertyDescriptor[] propertyDescriptors, java.lang.Object o, java.lang.String s) throws BeansException {
+        System.out.println("postProcessPropertyValues 方法调用");
+        return null;
+    }
+
+    @java.lang.Override
+    public java.lang.Object postProcessBeforeInitialization(java.lang.Object o, java.lang.String s) throws BeansException {
+        System.out.println("postProcessBeforeInitialization 方法调用");
+        return null;
+    }
+
+    @java.lang.Override
+    public java.lang.Object postProcessAfterInitialization(java.lang.Object o, java.lang.String s) throws BeansException {
+        System.out.println("postProcessAfterInitialization 方法调用");
+        return null;
     }
 }
